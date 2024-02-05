852a9698f75c530300955520dbbcf09a11f172d6
DAGO <christiandavidgomez466@gmail.com>
2024-02-05 17:09:46 -0500

    siii

--- src/index.mjs
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import "./database";
-
-import { PORT } from "./config";
-
-require("dotenv").config();
-
-const port = PORT;
-app.listen(port, console.log("Conectado al puerto ", port));
