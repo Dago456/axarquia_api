ae108074a19c1c8ed7c78c4b13b0de65d17312ee
DAGO <christiandavidgomez466@gmail.com>
2024-05-02 16:25:59 -0500

    se corrige una cagadota

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
 import comunidadesRoutes from "./comunidadesRoutes";
 import operariosRoutes from "./operariosRoutes";
-import registrarEntController from "./registroEntRoutes";
-import registrarSalController from "./registroSalRoutes";
+import registroEntRoutes from "./registroEntRoutes";
+import registroSalRoutes from "./registroSalRoutes";
 
 
 export default function (app) {
 
   app.use("/comunidad", comunidadesRoutes);
   app.use("/operario", operariosRoutes);
-  app.use("/registroent", registrarEntController)
-  app.use("/registroesal", registrarSalController)
+  app.use("/registroent", registroEntRoutes)
+  app.use("/registroesal", registroSalRoutes)
 
 }
