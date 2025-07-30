c4a0f1b8b3d25bb4e58b6ce5489d2401ed3e2724
Dago456 <christiandavidgomez466@gmail.com>
2025-07-29 22:23:55 -0500

    change any

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,13 +7,11 @@ import partesRoutes from "./partesRoutes";
 
 export default function (app) {
 
+  app.use("/partes", partesRoutes)
   app.use("/comunidad", comunidadesRoutes);
   app.use("/operario", operariosRoutes);
   app.use("/entrada", entradaRoutes)
   app.use("/salida", salidaRoutes)
-  console.log("✅ Agregando rutas de partes");
-
-  app.use("/partes", partesRoutes)
   app.use((req, res) => {
     res.status(404).json({ message: "Ruta no encontrada" });
   });
