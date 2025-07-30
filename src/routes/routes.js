98e0ed2c4835b92e0feba4db1dbe4cb0b63ac98b
Dago456 <christiandavidgomez466@gmail.com>
2025-07-29 22:18:25 -0500

    verify

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,8 @@ export default function (app) {
   app.use("/operario", operariosRoutes);
   app.use("/entrada", entradaRoutes)
   app.use("/salida", salidaRoutes)
+  console.log("✅ Agregando rutas de partes");
+
   app.use("/partes", partesRoutes)
   app.use((req, res) => {
     res.status(404).json({ message: "Ruta no encontrada" });
